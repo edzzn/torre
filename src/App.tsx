fb30f600714abcbb9ca56c3ac78c6faf8302c8c1
Edisson Reinozo <edisson.reinozo@ucuenca.edu.ec>
2019-12-03 01:10:29 -0500

    Styled components and default font

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import User from './features/User';
 import Search from './features/Search';
+import { AppContainer } from './styles';
 
 const history = createBrowserHistory();
 
 const App: React.FC = () => {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact={true} path='/user/:userId' component={User} />
-        <Route path='/' component={Search} />
-      </Switch>
-    </Router>
+    <AppContainer>
+      <Router history={history}>
+        <Switch>
+          <Route exact={true} path='/user/:userId' component={User} />
+          <Route path='/' component={Search} />
+        </Switch>
+      </Router>
+    </AppContainer>
   );
 };
 
