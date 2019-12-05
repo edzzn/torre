5762aeda09fa97f6891181c41d5dabc44dd57a9e
Edisson Reinozo <edisson.reinozo@ucuenca.edu.ec>
2019-12-04 21:17:53 -0500

    Better format

--- src/features/Search/SearchSideBar/styles.ts
+++ src/features/Search/SearchSideBar/styles.ts
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
-// import { COLOR_PALETTE } from '../../../common/colors';
 
 export const SearchSidebarContainer = styled.div`
+  display: block;
+  position: fixed;
+  margin-top: 56px;
+  margin-bottom: 56px;
+  height: 100%;
+  width: 300px;
+`;
+
+export const SearchColumn = styled.div`
   display: flex;
-  flex: 1;
-  padding: 1rem;
   flex-direction: column;
   overflow-y: scroll;
+  padding: 0.05rem;
+  height: calc(100% - 56px);
 `;
